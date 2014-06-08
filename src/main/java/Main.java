b99ef6f6a179a0f0eba9f979e658d75f120edc85
tomas <tomasabril@users.noreply.github.com>
2014-06-08 15:15:14 -0300

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * IF62C - Fundamentos de Programação 2
+ * 
+ * Template de projeto de programa Java usando Maven.
+ * @author Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
+ * modificado por Tomás Abril
+ */
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Olá, Java!");
+    }
+}
